33ca9d80655ec1f935f2417ef761e6319419e442
shacharfrk12 <128513872+shacharfrk12@users.noreply.github.com>
2024-08-23 05:35:33 +0300

    Update advanced_analysis_utils.py
    last updated version

--- advanced_analysis_utils.py
+++ advanced_analysis_utils.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import scipy.stats
+import statsmodels.api as sm
 
 # box plots 
 
@@ -31,42 +33,47 @@ def plot_boxes_readability(data, measure_list):
 
 
 def get_lin_stats(data: pd.DataFrame, eye_measure:str, readability_measure:str):
-    #eye_measure_diff = data[eye_measure+'_adv'] - data[eye_measure+'_ele']
     eye_measure_diff = np.array(data[eye_measure + '_adv']) - np.array(data[eye_measure + '_ele'])
     eye_measure_diff = sm.add_constant(eye_measure_diff)
     diff_readability = data[readability_measure]
-    lin_model = sm.OLS(diff_readability,eye_measure_diff, missing='drop').fit()
+    lin_model = sm.OLS(diff_readability, eye_measure_diff, missing='drop').fit()
 
     return lin_model.summary()
 
 #bar plots
-def plot_correclations(aligned_data:pd.DataFrame, measures:list, readability_measure:str, correlation_func = scipy.stats.pearsonr):
+def plot_correlations(aligned_data:pd.DataFrame, measures:list, readability_measure:str,measure_names:list,
+                      correlation_func=scipy.stats.pearsonr, with_values=False):
     aligned_data = aligned_data.dropna()
     diff_readability = aligned_data[readability_measure].to_numpy()
     corr_list = []
     r_list = []
     for eye_measure in measures:
         eye_measure_diff = np.array(aligned_data[eye_measure + '_adv']) - np.array(aligned_data[eye_measure + '_ele'])
-        #eye_measure_diff = aligned_data.apply(lambda row: row[eye_measure + '_ele'] - row[eye_measure + '_adv'],
-        # axis=1)
 
         corr, r = correlation_func(diff_readability, eye_measure_diff)
         corr_list.append(corr)
         r_list.append(r)
-    print(corr_list)
-    plt.bar(range(len(corr_list)), corr_list, tick_label=measures)
+
+    plt.bar(range(len(corr_list)), corr_list, tick_label=measure_names)
+
+    if with_values:
+        for i in range(len(corr_list)):
+            plt.text(i, corr_list[i], "{:.4f}".format(corr_list[i]), ha='center')
+
+    plt.title(readability_measure)
     plt.show()
 
 
 # relationships
-def plot_scatter(data: pd.DataFrame, eye_measure:str, readability_measure:str):
+def plot_scatter(data: pd.DataFrame, eye_measure: str, readability_measure: str, eye_measure_name: str,
+                 readability_name:str):
     eye_measure_diff = np.array(data[eye_measure + '_adv']) - np.array(data[eye_measure + '_ele'])
-    #diff_readability = data[readability_measure]
     data[eye_measure+"_diff"] = eye_measure_diff
-    #present_df = pd.DataFrame()
-    #present_df['measure'] = eye_measure_diff
-    #present_df['readability'] = diff_readability
     sns.regplot(x=eye_measure+"_diff", y=readability_measure, data=data, x_bins=20)
+    plt.xlabel(eye_measure_name)
+    plt.ylabel(readability_name)
+    plt.figure(constrained_layout=True)
+    plt.title(f"{readability_name} as function of {eye_measure_name}")
     plt.show()
 
     g = sns.jointplot(x=eye_measure+"_diff", y=readability_measure, data=data, kind='reg')
@@ -76,4 +83,19 @@ def plot_scatter(data: pd.DataFrame, eye_measure:str, readability_measure:str):
     plt.show()
 
 
+def lin_model_multi(data, eye_measures, readability_measure):
+    # Fit and summarize OLS model
+    eye_measures_diff_dict = {}
 
+    for eye_measure in eye_measures:
+        eye_measures_diff_dict[eye_measure] = np.array(data[eye_measure + '_adv']) - np.array(data[eye_measure +
+                                                                                                   '_ele'])
+
+    X = pd.DataFrame.from_dict(eye_measures_diff_dict)
+    X = sm.add_constant(X)
+
+    readability_diff = data[[readability_measure]]
+
+    lin_model = sm.OLS(readability_diff, X, missing='drop').fit()
+
+    print(lin_model.summary())
